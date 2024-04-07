7fb86dcaeb390a5910c5030aa544e11cedaa096f
StrangerDeer <karmanb96@gmail.com>
2024-04-07 13:49:09 +0200

    Add HexDecoder.cs

---
+++ ImageProcessing/Converters/HexDecoder.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PictureProcessing.Converters;
+
+public class HexDecoder : IDecodeToByteArr
+{
+    public bool IsDecode(string image)
+    {
+        try
+        {
+            var valid = Convert.FromHexString(image);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public byte[] Decode(string image)
+    {
+        return Convert.FromHexString(image);
+    }
+}
