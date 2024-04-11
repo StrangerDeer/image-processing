624a6204fda771960874ec5a426d5bc7946d533e
StrangerDeer <karmanb96@gmail.com>
2024-04-12 00:59:01 +0200

    finish

--- ImageProcessing/imagesettingcpp/library.cpp
+++ ImageProcessing/imagesettingcpp/library.cpp
@@ -3,6 +3,21 @@
 #include <omp.h>
 #include <opencv2/imgproc.hpp>
 
+///<summary>
+/// Slices the input image into multiple segments for parallel processing.
+///</summary>
+///<param name="inputImage">Input image to be sliced.</param>
+///<param name="height">Height of the input image.</param>
+///<param name="numThread">Number of threads used for parallel processing.</param>
+///<param name="colsPerThread">Number of columns per thread.</param>
+///<param name="remainingCols">Number of remaining columns after evenly distributing columns per thread.</param>
+///<returns>Vector of image slices representing segments of the input image.</returns>
+///<remarks>
+/// This function divides the input image into multiple segments, each representing a portion of the input image.
+/// The number of segments is determined by the numThread parameter, and each segment contains an equal number of columns,
+/// except for the last segment, which may have additional columns if the total number of columns is not divisible evenly.
+/// The height of each segment is equal to the height of the input image.
+///</remarks>
 std::vector<cv::Mat> sliceImage(const cv::Mat& inputImage, int height, int numThread, int colsPerThread, int remainingCols) {
   std::vector<cv::Mat> imageSlices;
 
@@ -29,6 +44,19 @@ std::vector<cv::Mat> sliceImage(const cv::Mat& inputImage, int height, int numTh
   return imageSlices;
 }
 
+///<summary>
+/// Applies Gaussian blur to each segment of the input image slices and accumulates the results into the output image using OpenMP for parallel processing.
+///</summary>
+///<param name="imageSlices">Vector of input image slices.</param>
+///<param name="gaussian">Output image where the blurred image slices will be accumulated.</param>
+///<param name="colsPerThread">Number of columns per thread.</param>
+///<param name="height">Height of the input image.</param>
+///<param name="blurValue">Value determining the intensity of the blur.</param>
+///<remarks>
+/// This function applies Gaussian blur to each segment of the input image slices using OpenMP to parallelize the process.
+/// The resulting blurred image slices are accumulated into the output image.
+/// The blur intensity is determined by the blurValue parameter.
+///</remarks>
 void makeGaussianBlur(const std::vector<cv::Mat>& imageSlices, cv::Mat& gaussian, int colsPerThread, int height, int blurValue) {
 #pragma omp parallel for
   for (int i = 0; i < imageSlices.size(); ++i) {
--- ImageProcessing/imagesettingcpp/library.h
+++ ImageProcessing/imagesettingcpp/library.h
@@ -4,22 +4,20 @@
 #include <vector>
 #include <opencv2/core.hpp>
 
-/// <summary>
-/// Applies Gaussian blur to an RGBA image.
-/// </summary>
-/// <param name="image">Pointer to the RGBA image data.</param>
-/// <param name="width">Width of the image.</param>
-/// <param name="height">Height of the image.</param>
-/// <param name="blurValue">Size of the Gaussian kernel for blurring.
-/// The value must be an odd number.  </param>
-/// <param name="result">Pointer to store the resulting blurred image data.
+///<summary>
+/// Applies Gaussian blur to an input image using OpenMP for parallel processing.
+///</summary>
+///<param name="image">Pointer to the input image data.</param>
+///<param name="width">Width of the input image.</param>
+///<param name="height">Height of the input image.</param>
+///<param name="blurValue">Value determining the intensity of the blur.</param>
+///<param name="result">Pointer to the buffer where the blurred image data will be stored.
 /// The length of the result has to be the same size as the image pointer</param>
-/// <remarks>
-/// This function applies Gaussian blur to an RGBA image represented by the input image data.
-/// It uses OpenCV's GaussianBlur function to perform the blurring operation.
-/// The OpenCV GaussianBlur function appears to run in multithreading mode.
-/// I measured its runtime.
-/// When I use OMP parallelization, the runtime is longer compared to when I only call the GaussianBlur function.
-/// </remarks>
+///<remarks>
+/// This function takes an input image represented as a pointer to image data in memory and its dimensions.
+/// It applies Gaussian blur to the input image using OpenMP to parallelize the process.
+/// The blur intensity is determined by the blurValue parameter.
+/// The resulting blurred image data is stored in the result buffer.
+///</remarks>
 extern "C" __declspec(dllexport) void gaussianBlur(uint8_t* image, int width, int height, int blurValue, uint8_t* result);
 
